82d3cbc998915ec5956a8cb7c1bf55330622cf41
Toby Osbourn <toby.osbourn@googlemail.com>
2018-02-22 12:23:55 +0000

    Adds via: :all to match commands

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Mercury::Engine.routes.draw do
-  match '/editor(/*requested_uri)' => "mercury#edit", :as => :mercury_editor
+  match '/editor(/*requested_uri)' => "mercury#edit", :as => :mercury_editor, via: :all
 
   scope '/mercury' do
-    match ':type/:resource' => "mercury#resource"
-    match 'snippets/:name/options' => "mercury#snippet_options"
-    match 'snippets/:name/preview' => "mercury#snippet_preview"
+    match ':type/:resource' => "mercury#resource", via: :all
+    match 'snippets/:name/options' => "mercury#snippet_options", via: :all
+    match 'snippets/:name/preview' => "mercury#snippet_preview", via: :all
   end
 end
